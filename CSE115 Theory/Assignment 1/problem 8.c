2514a06b2a9f19f35b9b6ed9401ae8d4374b7cb7
Tryodoxx <farhan0eshrak@gmail.com>
2025-08-16 19:24:01 +0600

    Create problem 8.c

---
+++ CSE115 Theory/Assignment 1/problem 8.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+
+int collatz(int n){
+    int count=1;
+    if(n<=0){
+        printf("Undefined");
+    }
+    while(n!=1){
+        if (n%2==0){
+            n=n/2;
+        } else if(n%2!=0) {
+            n=n*3+1;
+        } count++;
+    }
+    return count;
+}
+
+
+int main() {
+    int n,result;
+    printf("Enter a Number: ");
+    scanf("%d",&n);
+    result=collatz(n);
+    printf("%d",result);
+    return 0;
+}
