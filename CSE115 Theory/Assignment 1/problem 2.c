1c90e225e70fe3df4f7be71fe52b4421861ad884
Tryodoxx <farhan0eshrak@gmail.com>
2025-08-16 19:22:35 +0600

    Create problem 2.c

---
+++ CSE115 Theory/Assignment 1/problem 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sumOfDigits(int num){
+    int digit,sum=0;
+    if (num<0){
+        num=-num;
+    }
+    while(num!=0){
+        digit=num%10;
+        num=num/10;
+        sum=sum+digit;
+    }  return sum;
+}
+
+int main() {
+    int num,digit,sum;
+    scanf("%d",&num);
+    sum = sumOfDigits(num);
+    printf("%d",sum);
+
+    return 0;
+}
